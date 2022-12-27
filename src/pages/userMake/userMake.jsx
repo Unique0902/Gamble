38283431dd8b6f4004dd384679b680827ccf2abc
mook <rla0591@gmail.com>
2022-12-27 17:41:14 +0900

    make userMake function

--- src/pages/userMake/userMake.jsx
+++ src/pages/userMake/userMake.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
 import DesignWrapper from '../../components/designWrapper/designWrapper';
-import { useAuthContext } from '../../context/AuthContext';
+import { useUserDataContext } from '../../context/UserDataContext';
 import styles from './userMake.module.css';
 
 export default function UserMake() {
-  const [name, setName] = useState();
-  const { uid } = useAuthContext();
+  const [name, setName] = useState('');
+  const { makeUserData } = useUserDataContext();
   const handleSubmit = (e) => {
     e.preventDefault();
+    makeUserData(name);
+  };
+  const handleChange = (e) => {
+    setName(e.target.value);
   };
   return (
     <DesignWrapper>
       <section className={styles.titleSec}>
         <h1 className={styles.title}>유저 생성</h1>
         <form onSubmit={handleSubmit}>
-          <input type='text' />
+          <input
+            placeholder='이름 입력..'
+            type='text'
+            value={name}
+            name='name'
+            required
+            onChange={handleChange}
+          />
           <button>제출</button>
         </form>
       </section>
