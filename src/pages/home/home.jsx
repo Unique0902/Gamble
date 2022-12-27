79eb209dffdb5db9f66119856ca2d3546623652b
mook <rla0591@gmail.com>
2022-12-27 14:00:41 +0900

    add react-icon module

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './home.module.css';
+import { FcGoogle } from 'react-icons/fc';
 export default function Home() {
   return (
     <div className={styles.wrap}>
@@ -29,7 +30,10 @@ export default function Home() {
       <section className={styles.titleSec}>
         <h1 className={styles.title}>Gamble</h1>
         <h2 className={styles.subTitle}>부자되세요 ^^</h2>
-        <button className={styles.btn}>Google 로그인</button>
+        <button className={styles.btn}>
+          <FcGoogle />
+          Google 로그인
+        </button>
       </section>
       <div class='custom-shape-divider-bottom-1672116618'>
         <svg
