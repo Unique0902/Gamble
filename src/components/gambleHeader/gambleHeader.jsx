f4c659054fc52ea9ce8ec2df63cfaaecffdeca59
mook <rla0591@gmail.com>
2022-12-30 14:42:21 +0900

    add gambleHeader selectedStyle and hoverStyle

--- src/components/gambleHeader/gambleHeader.jsx
+++ src/components/gambleHeader/gambleHeader.jsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './gambleHeader.module.css';
 
 export default function GambleHeader() {
+  const [selectedBtn, setSelectedBtn] = useState('');
+  let location = useLocation();
+  const checkSelectedBtn = () => {
+    const pathArr = location.pathname.split('/');
+    setSelectedBtn(pathArr[2]);
+  };
+  useEffect(() => {
+    checkSelectedBtn();
+  }, [location]);
+  const selectedStyle = (name) => {
+    return name === selectedBtn && styles.selected;
+  };
   return (
     <header className={styles.header}>
       <div className={styles.title}>
         <span className={styles.titleText}>Gamble</span>
       </div>
-      <Link to='snail' className={styles.gamble}>
+      <Link to='snail' className={`${styles.gamble} ${selectedStyle('snail')}`}>
         달팽이
       </Link>
-      <Link to='sniffling' className={styles.gamble}>
+      <Link
+        to='sniffling'
+        className={`${styles.gamble} ${selectedStyle('sniffling')}`}
+      >
         홀짝
       </Link>
-      <Link to='graph' className={styles.gamble}>
+      <Link to='graph' className={`${styles.gamble} ${selectedStyle('graph')}`}>
         그래프
       </Link>
-      <Link to='roulette' className={styles.gamble}>
+      <Link
+        to='roulette'
+        className={`${styles.gamble} ${selectedStyle('roulette')}`}
+      >
         룰렛
       </Link>
-      <Link to='swordReinforce' className={styles.gamble}>
+      <Link
+        to='swordReinforce'
+        className={`${styles.gamble} ${selectedStyle('swordReinforce')}`}
+      >
         검강화
       </Link>
     </header>
